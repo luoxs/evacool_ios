49b067ff13a728514c18977e2d651c3af51edf44
xsluo <xsluo@126.com>
2023-12-28 19:34:12 +0800

    3# 主界面基本完成

--- Evacool/TruckViewController.m
+++ Evacool/TruckViewController.m
@@ -40,7 +40,7 @@
     .widthIs(228.0/frameWidth*viewX)
     .heightIs(82.0/frameHeight*viewY);
    
-    //坐上文字1
+    //左上文字1
     UILabel *labelUp = [UILabel new];
     [self.view addSubview:labelUp];
     labelUp.text = @"EVA 24V";
@@ -52,7 +52,7 @@
     .widthIs(300.0/frameWidth*viewX)
     .heightIs(38.0/frameHeight*viewY);
     
-    //中间文字下
+    //左上文字2
     UILabel *labelDown = [UILabel new];
     [self.view addSubview:labelDown];
     labelDown.text = @"Truck Air Conditioner";
@@ -64,20 +64,75 @@
     .widthIs(600.0/frameWidth*viewX)
     .heightIs(38.0/frameHeight*viewY);
     
+    //开关
+    UIButton *btPower = [UIButton new];
+    [btPower setBackgroundImage:[UIImage imageNamed:@"power"] forState:UIControlStateNormal];
+    [self.view addSubview:btPower];
+    btPower.sd_layout
+    .rightSpaceToView(self.view, 36.0/frameWidth*viewX)
+    .topSpaceToView(self.view, 222.0/frameHeight*viewY)
+    .widthIs(120.0/frameWidth*viewX)
+    .heightEqualToWidth();
     
-    /*
-    
-    //首页图片
-    UIImageView *imageHome = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"imagehome"]];
-    [self.view addSubview:imageHome];
-    imageHome.sd_layout
+    //圆形背景
+    UIView *view0 = [UIView new];
+    [self.view addSubview:view0];
+    [view0 setBackgroundColor:[UIColor whiteColor]];
+    view0.sd_layout
     .centerXEqualToView(self.view)
-    .topSpaceToView(self.view, 210.0/frameHeight*viewY)
-    .widthIs(680.0/frameWidth*viewX)
-    .heightIs(360.0/frameHeight*viewY);
-    [imageHome setSd_cornerRadius:@10.0];
-    */
+    .topSpaceToView(self.view, 472.0/frameHeight*viewY)
+    .widthIs(380.0/frameWidth*viewX)
+    .heightEqualToWidth();
+    view0.layer.cornerRadius = view0.width * 0.5;
+    view0.layer.masksToBounds = true;
     
+    //实时温度
+    UILabel *labelTemp= [UILabel new];
+    [self.view addSubview:labelTemp];
+    labelTemp.text = @"0°C";
+    [labelTemp setTextAlignment:NSTextAlignmentCenter];
+    [labelTemp setFont:[UIFont fontWithName:@"Arial" size:36.0]];
+    labelTemp.sd_layout
+    .centerXEqualToView(self.view)
+    .centerYEqualToView(view0)
+    .widthIs(200.0/frameWidth*viewX)
+    .heightIs(52.0/frameHeight*viewY);
+    
+    //状态
+    UILabel *labelStatus= [UILabel new];
+    [self.view addSubview:labelStatus];
+    labelStatus.text = @"In Good Condition";
+    [labelStatus setTextAlignment:NSTextAlignmentCenter];
+    [labelStatus setFont:[UIFont fontWithName:@"Arial" size:12.0]];
+    labelStatus.sd_layout
+    .centerXEqualToView(self.view)
+    .topSpaceToView(labelTemp, 30.0/frameHeight*viewY)
+    .widthIs(600.0/frameWidth*viewX)
+    .heightIs(12.0/frameHeight*viewY);
+    
+    //温度减
+    UIButton *btTempMinus = [UIButton new];
+    [self.view addSubview:btTempMinus];
+    [btTempMinus setBackgroundImage:[UIImage imageNamed:@"minus"] forState:UIControlStateNormal];
+    btTempMinus.sd_layout
+    .leftSpaceToView(self.view, 210.0/frameWidth*viewX)
+    .bottomEqualToView(labelTemp)
+    .widthIs(54.0/frameWidth*viewX)
+    .heightEqualToWidth();
+    
+    
+    //温度加
+    UIButton *btTempAdd = [UIButton new];
+    [btTempAdd setBackgroundImage:[UIImage imageNamed:@"add"] forState:UIControlStateNormal];
+    [self.view addSubview:btTempAdd];
+    btTempAdd.sd_layout
+    .rightSpaceToView(self.view, 210.0/frameWidth*viewX)
+    .bottomEqualToView(labelTemp)
+    .widthIs(54.0/frameWidth*viewX)
+    .heightEqualToWidth();
+    
+    
+#pragma mark 显示风速
     //一个水平视图
     UIView *view1 = [UIView new];
     [view1 setBackgroundColor:[UIColor whiteColor]];
@@ -89,17 +144,64 @@
     .heightIs(168.0/frameHeight*viewY);
     [view1 setSd_cornerRadius:@10.0];
     
+    UILabel *labelscale = [UILabel new];
+    [view1 addSubview:labelscale];
+    labelscale.text = @"1-5";
+    [labelscale setTextAlignment:NSTextAlignmentLeft];
+    [labelscale setTextColor:[UIColor grayColor]];
+    [labelscale setFont:[UIFont fontWithName:@"Arial" size:12.0]];
+    labelscale.sd_layout
+    .leftSpaceToView(view1, 74.0/frameWidth*viewX)
+    .topSpaceToView(view1, 32.0/frameHeight*viewY)
+    .widthIs(100.0/frameWidth*viewX)
+    .heightIs(30.0/frameHeight*viewY);
     
-    //风速
-    UIImageView *imageSpeed = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"speed1"]];
-    [view1 addSubview:imageSpeed];
-    imageSpeed.sd_layout
-    .centerXEqualToView(view1)
-    .widthIs(552.0/frameWidth*viewX)
-    .topSpaceToView(view1, 82.0/frameHeight*viewY)
+    //风速1
+    UIImageView *imgfan1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"d1"]];
+    [view1 addSubview:imgfan1];
+    imgfan1.sd_layout
+    .leftSpaceToView(view1, 64.0/frameWidth*viewX)
+    .bottomSpaceToView(view1, 40.0/frameHeight*viewY)
+    .widthIs(92.0/frameWidth*viewX)
+    .heightIs(16.0/frameHeight*viewY);
+    
+    //风速2
+    UIImageView *imgfan2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"d2"]];
+    [view1 addSubview:imgfan2];
+    imgfan2.sd_layout
+    .leftSpaceToView(view1, 178.0/frameWidth*viewX)
+    .bottomSpaceToView(view1, 40.0/frameHeight*viewY)
+    .widthIs(92.0/frameWidth*viewX)
+    .heightIs(26.0/frameHeight*viewY);
+    
+    //风速3
+    UIImageView *imgfan3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"d3"]];
+    [view1 addSubview:imgfan3];
+    imgfan3.sd_layout
+    .leftSpaceToView(view1, 294.0/frameWidth*viewX)
+    .bottomSpaceToView(view1, 40.0/frameHeight*viewY)
+    .widthIs(92.0/frameWidth*viewX)
+    .heightIs(36.0/frameHeight*viewY);
+    
+    //风速4
+    UIImageView *imgfan4 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"d4"]];
+    [view1 addSubview:imgfan4];
+    imgfan4.sd_layout
+    .leftSpaceToView(view1, 410.0/frameWidth*viewX)
+    .bottomSpaceToView(view1, 40.0/frameHeight*viewY)
+    .widthIs(92.0/frameWidth*viewX)
+    .heightIs(46.0/frameHeight*viewY);
+    
+    //风速5
+    UIImageView *imgfan5 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"d5"]];
+    [view1 addSubview:imgfan5];
+    imgfan5.sd_layout
+    .leftSpaceToView(view1, 524.0/frameWidth*viewX)
+    .bottomSpaceToView(view1, 40.0/frameHeight*viewY)
+    .widthIs(92.0/frameWidth*viewX)
     .heightIs(56.0/frameHeight*viewY);
     
-    
+#pragma mark 显示睡眠定时
     //视图2
     UIView *view2 = [UIView new];
     [view2 setBackgroundColor:[UIColor whiteColor]];
@@ -111,7 +213,64 @@
     .heightIs(168.0/frameHeight*viewY);
     [view1 setSd_cornerRadius:@10.0];
     
+    UILabel *labelsleep= [UILabel new];
+    [view2 addSubview:labelsleep];
+    labelsleep.text = @"0.5-10h";
+    [labelsleep setTextAlignment:NSTextAlignmentLeft];
+    [labelsleep setTextColor:[UIColor grayColor]];
+    [labelsleep setFont:[UIFont fontWithName:@"Arial" size:12.0]];
+    labelsleep.sd_layout
+    .leftSpaceToView(view2, 74.0/frameWidth*viewX)
+    .topSpaceToView(view2, 32.0/frameHeight*viewY)
+    .widthIs(100.0/frameWidth*viewX)
+    .heightIs(30.0/frameHeight*viewY);
     
+    //开关
+    UISwitch *switchSleep = [UISwitch new];
+    [view2 addSubview:switchSleep];
+    switchSleep.sd_layout
+    .rightSpaceToView(view2, 22.0/frameWidth*viewX)
+    .topSpaceToView(view2, 22.0/frameHeight*viewY)
+    .widthIs(94.0/frameWidth*viewX)
+    .heightIs(36.0/frameHeight*viewY);
+    
+    //定时量
+    UILabel *labelTimer= [UILabel new];
+    [view2 addSubview:labelTimer];
+    labelTimer.text = @"0.5h";
+    [labelTimer setTextAlignment:NSTextAlignmentLeft];
+    [labelTimer setFont:[UIFont fontWithName:@"Arial" size:20.0]];
+    [labelTimer setTextAlignment:NSTextAlignmentCenter];
+    labelTimer.sd_layout
+    .centerXEqualToView(view2)
+    .widthIs(100.0/frameWidth*viewX)
+    .heightIs(40.0/frameHeight*viewY)
+    .bottomSpaceToView(view2, 34.0/frameHeight*viewY);
+    
+    //定时减
+    UIButton *btTimeMinus = [UIButton new];
+    [btTimeMinus setBackgroundImage:[UIImage imageNamed:@"minus"] forState:UIControlStateNormal];
+    [view2 addSubview:btTimeMinus];
+    btTimeMinus.sd_layout
+    .leftSpaceToView(view2, 98.0/frameWidth*viewX)
+    .centerYEqualToView(labelTimer)
+    .widthIs(42.0/frameWidth*viewX)
+    .heightEqualToWidth();
+    
+    
+    //定时加
+    UIButton *btTimeAdd = [UIButton new];
+    [btTimeAdd setBackgroundImage:[UIImage imageNamed:@"add"] forState:UIControlStateNormal];
+    [view2 addSubview:btTimeAdd];
+    btTimeAdd.sd_layout
+    .rightSpaceToView(view2, 98.0/frameWidth*viewX)
+    .centerYEqualToView(labelTimer)
+    .widthIs(42.0/frameWidth*viewX)
+    .heightEqualToWidth();
+    
+    
+    
+#pragma  mark 显示电池
     UIView *view3 = [UIView new];
     [view3 setBackgroundColor:[UIColor whiteColor]];
     [self.view addSubview:view3];
@@ -125,7 +284,7 @@
     
     //电池
     UIButton *buttonBattry = [UIButton new];
-    [buttonBattry setBackgroundImage:[UIImage imageNamed:@"battery"] forState:UIControlStateNormal];
+    [buttonBattry setBackgroundImage:[UIImage imageNamed:@"batteryoff"] forState:UIControlStateNormal];
     [view3 addSubview:buttonBattry];
     buttonBattry.sd_layout
     .centerXEqualToView(view3)
@@ -133,52 +292,70 @@
     .widthIs(82.0/frameWidth*viewX)
     .heightEqualToWidth();
     
+    //风量
+    UIButton *btFan = [UIButton new];
+    [self.view addSubview:btFan];
+    [btFan setBackgroundImage:[UIImage imageNamed:@"fanoff"] forState:UIControlStateNormal];
+    btFan.sd_layout
+    .leftSpaceToView(self.view, 44.0/frameWidth*viewX)
+    .topSpaceToView(self.view, 1304.0/frameHeight*viewY)
+    .widthIs(122.0/frameWidth*viewX)
+    .autoHeightRatio(142.0/122.0);
     
-    /*
+    //节能
+    UIButton *btEco = [UIButton new];
+    [self.view addSubview:btEco];
+    [btEco setBackgroundImage:[UIImage imageNamed:@"ecooff"] forState:UIControlStateNormal];
+    btEco.sd_layout
+    .leftSpaceToView(self.view, 222.0/frameWidth*viewX)
+    .topSpaceToView(self.view, 1304.0/frameHeight*viewY)
+    .widthIs(122.0/frameWidth*viewX)
+    .autoHeightRatio(142.0/122.0);
     
+    //普通模式
+    UIButton *btNormal = [UIButton new];
+    [self.view addSubview:btNormal];
+    [btNormal setBackgroundImage:[UIImage imageNamed:@"cooloff"] forState:UIControlStateNormal];
+    btNormal.sd_layout
+    .leftSpaceToView(self.view, 402.0/frameWidth*viewX)
+    .topSpaceToView(self.view, 1304.0/frameHeight*viewY)
+    .widthIs(122.0/frameWidth*viewX)
+    .autoHeightRatio(142.0/122.0);
     
-    //底部logo1
-    UIImageView *imageLogo1 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"germany"]];
-    [self.view addSubview:imageLogo1];
-    imageLogo1.sd_layout
-    .leftSpaceToView(self.view, 116.0/frameWidth*viewX)
-    .topSpaceToView(self.view, 1440.0/frameHeight*viewY)
-    .widthIs(154.0/frameWidth*viewX)
-    .heightIs(84.0/frameHeight*viewY);
+    //加强模式
+    UIButton *btTurbo = [UIButton new];
+    [self.view addSubview:btTurbo];
+    [btTurbo setBackgroundImage:[UIImage imageNamed:@"turbooff"] forState:UIControlStateNormal];
+    btTurbo.sd_layout
+    .rightSpaceToView(self.view, 44.0/frameWidth*viewX)
+    .topSpaceToView(self.view, 1304.0/frameHeight*viewY)
+    .widthIs(122.0/frameWidth*viewX)
+    .autoHeightRatio(142.0/122.0);
     
-    //底部logo2
-    UIImageView *imageLogo2 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"instein"]];
-    [self.view addSubview:imageLogo2];
-    imageLogo2.sd_layout
-    .rightSpaceToView(self.view, 116.0/frameWidth*viewX)
-    .topSpaceToView(self.view, 1440.0/frameHeight*viewY)
-    .widthIs(154.0/frameWidth*viewX)
-    .heightIs(84.0/frameHeight*viewY);
+    //底部左边按钮
+    UIButton *buttonDetails = [UIButton new];
+    [buttonDetails setBackgroundColor:[UIColor colorWithRed:29.0/255 green:130.0/255 blue:254.0/255 alpha:1.0]];
+    [buttonDetails setTitle:@"Details" forState:UIControlStateNormal];
+    [self.view addSubview:buttonDetails];
+    buttonDetails.sd_layout
+    .leftSpaceToView(self.view, 124.0/frameWidth*viewX)
+    .topSpaceToView(self.view, 1508.0/frameHeight*viewY)
+    .widthIs(226.0/frameWidth*viewX)
+    .heightIs(70.0/frameHeight*viewY);
+    [buttonDetails setSd_cornerRadius:@12.0];
     
-    UILabel *labelEva = [UILabel new];
-    [self.view addSubview:labelEva];
-    labelEva.text = @"www.evacool.com.tr";
-    [labelEva setTextColor:[UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0]];
-    [labelEva setTextAlignment:NSTextAlignmentCenter];
-    [labelEva setFont:[UIFont fontWithName:@"Arial" size:12.0]];
-    labelEva.sd_layout
-    .centerXEqualToView(imageLogo1)
-    .topSpaceToView(imageLogo1, 10.0/frameHeight*viewY)
-    .widthIs(300.0/frameWidth*viewX)
-    .heightIs(20.0/frameHeight*viewY);
+    //底部左边按钮
+    UIButton *buttonFaults = [UIButton new];
+    [buttonFaults setBackgroundColor:[UIColor colorWithRed:29.0/255 green:130.0/255 blue:254.0/255 alpha:1.0]];
+    [buttonFaults setTitle:@"Faults Record" forState:UIControlStateNormal];
+    [self.view addSubview:buttonFaults];
+    buttonFaults.sd_layout
+    .rightSpaceToView(self.view, 124.0/frameWidth*viewX)
+    .topSpaceToView(self.view, 1508.0/frameHeight*viewY)
+    .widthIs(226.0/frameWidth*viewX)
+    .heightIs(70.0/frameHeight*viewY);
+    [buttonFaults setSd_cornerRadius:@12.0];
     
-    UILabel *labeleinstein = [UILabel new];
-    [self.view addSubview:labeleinstein];
-    labeleinstein.text = @"www.einsteinmobilehomes.de";
-    [labeleinstein setTextColor:[UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0]];
-    [labeleinstein setTextAlignment:NSTextAlignmentCenter];
-    [labeleinstein setFont:[UIFont fontWithName:@"Arial" size:12.0]];
-    labeleinstein.sd_layout
-    .centerXEqualToView(imageLogo2)
-    .topSpaceToView(imageLogo2, 10.0/frameHeight*viewY)
-    .widthIs(400.0/frameWidth*viewX)
-    .heightIs(20.0/frameHeight*viewY);
-     */
     
 }
 
