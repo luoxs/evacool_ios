4527e5d83a71209d1c83134a4cb98227c2bd1951
xsluo <xsluo@126.com>
2024-01-21 17:58:05 +0800

    保存

--- Evacool/ProfileViewController.m
+++ Evacool/ProfileViewController.m
@@ -30,6 +30,35 @@
     double viewX = [UIScreen mainScreen].bounds.size.width;
     double viewY = [UIScreen mainScreen].bounds.size.height;
     
+    UIImageView *imgback = [UIImageView new];
+    [self.view addSubview:imgback];
+    [imgback setImage:[UIImage imageNamed:@"a1509"]];
+    imgback.sd_layout
+    .centerXEqualToView(self.view)
+    .widthRatioToView(self.view, 1.0)
+    .topSpaceToView(self.view, 0)
+    .autoHeightRatio(468.0/750);
+    
+    UIImageView *imgback1 = [UIImageView new];
+    [self.view addSubview:imgback1];
+    [imgback1 setImage:[UIImage imageNamed:@"a91"]];
+    imgback1.sd_layout
+    .leftSpaceToView(self.view, 50/frameWidth*viewX)
+    .widthRatioToView(self.view, 0.06)
+    .topSpaceToView(self.view, 368/frameHeight*viewY)
+    .widthEqualToHeight();
+    [imgback1 setContentMode:UIViewContentModeScaleAspectFit];
+    
+    UIImageView *imgback2 = [UIImageView new];
+    [self.view addSubview:imgback2];
+    [imgback2 setImage:[UIImage imageNamed:@""]];
+    imgback2.sd_layout
+    .centerXEqualToView(self.view)
+    .widthRatioToView(self.view, 1.0)
+    .topSpaceToView(self.view, 0)
+    .autoHeightRatio(468.0/750);
+    
+    
     //返回按钮
     UIButton *btBack = [UIButton new];
     [self.view addSubview:btBack];
@@ -51,10 +80,10 @@
     [self.view addSubview:labelname];
     [labelname setText:@"User Name"];
     [labelname setTextColor:[UIColor blackColor]];
-    [labelname setFont:[UIFont fontWithName:@"Arial" size:16]];
+    [labelname setFont:[UIFont fontWithName:@"Arial" size:14]];
     labelname.sd_layout
     .leftSpaceToView(self.view, 50/frameWidth*viewX)
-    .topSpaceToView(self.view, 650/frameHeight*viewY)
+    .topSpaceToView(self.view, 700/frameHeight*viewY)
     .widthIs(400/frameWidth*viewX)
     .heightIs(50/frameHeight*viewY);
     [labelname setTextAlignment:NSTextAlignmentLeft];
@@ -64,10 +93,10 @@
     [self.view addSubview:txtname];
     [txtname setText:@""];
     [txtname setTextColor:[UIColor blackColor]];
-    [txtname setFont:[UIFont fontWithName:@"Arial" size:16]];
+    [txtname setFont:[UIFont fontWithName:@"Arial" size:14]];
     txtname.sd_layout
-        .leftSpaceToView(self.view, 260/frameWidth*viewX)
-        .topSpaceToView(self.view, 650/frameHeight*viewY)
+        .leftSpaceToView(self.view, 310/frameWidth*viewX)
+        .topSpaceToView(self.view, 700/frameHeight*viewY)
         .widthIs(400/frameWidth*viewX)
         .heightIs(50/frameHeight*viewY);
     [txtname setTextAlignment:NSTextAlignmentLeft];
@@ -79,10 +108,10 @@
     [self.view addSubview:labelphone];
     [labelphone setText:@"Contact No."];
     [labelphone setTextColor:[UIColor blackColor]];
-    [labelphone setFont:[UIFont fontWithName:@"Arial" size:16]];
+    [labelphone setFont:[UIFont fontWithName:@"Arial" size:14]];
     labelphone.sd_layout
     .leftSpaceToView(self.view, 50/frameWidth*viewX)
-    .topSpaceToView(self.view, 780/frameHeight*viewY)
+    .topSpaceToView(self.view, 800/frameHeight*viewY)
     .widthIs(400/frameWidth*viewX)
     .heightIs(50/frameHeight*viewY);
     [labelphone setTextAlignment:NSTextAlignmentLeft];
@@ -91,10 +120,10 @@
     [self.view addSubview:txtphone];
     [txtphone setText:@""];
     [txtphone setTextColor:[UIColor blackColor]];
-    [txtphone setFont:[UIFont fontWithName:@"Arial" size:16]];
+    [txtphone setFont:[UIFont fontWithName:@"Arial" size:14]];
     txtphone.sd_layout
-        .leftSpaceToView(self.view, 260/frameWidth*viewX)
-        .topSpaceToView(self.view, 780/frameHeight*viewY)
+        .leftSpaceToView(self.view, 310/frameWidth*viewX)
+        .topSpaceToView(self.view, 800/frameHeight*viewY)
         .widthIs(400/frameWidth*viewX)
         .heightIs(50/frameHeight*viewY);
     [txtphone setTextAlignment:NSTextAlignmentLeft];
@@ -105,10 +134,10 @@
     [self.view addSubview:labelmail];
     [labelmail setText:@"Email"];
     [labelmail setTextColor:[UIColor blackColor]];
-    [labelmail setFont:[UIFont fontWithName:@"Arial" size:16]];
+    [labelmail setFont:[UIFont fontWithName:@"Arial" size:14]];
     labelmail.sd_layout
     .leftSpaceToView(self.view, 50/frameWidth*viewX)
-    .topSpaceToView(self.view, 910/frameHeight*viewY)
+    .topSpaceToView(self.view, 900/frameHeight*viewY)
     .widthIs(400/frameWidth*viewX)
     .heightIs(50/frameHeight*viewY);
     [labelmail setTextAlignment:NSTextAlignmentLeft];
@@ -117,32 +146,110 @@
     [self.view addSubview:txtmail];
     [txtmail setText:@""];
     [txtmail setTextColor:[UIColor blackColor]];
-    [txtmail setFont:[UIFont fontWithName:@"Arial" size:16]];
+    [txtmail setFont:[UIFont fontWithName:@"Arial" size:14]];
     txtmail.sd_layout
-        .leftSpaceToView(self.view, 260/frameWidth*viewX)
-        .topSpaceToView(self.view, 910/frameHeight*viewY)
+        .leftSpaceToView(self.view, 310/frameWidth*viewX)
+        .topSpaceToView(self.view, 900/frameHeight*viewY)
         .widthIs(400/frameWidth*viewX)
         .heightIs(50/frameHeight*viewY);
     [txtmail setTextAlignment:NSTextAlignmentLeft];
     [txtmail setText:self.email];
   
     
+    //product
+    UILabel *labelproduct = [UILabel new];
+    [self.view addSubview:labelproduct];
+    [labelproduct setText:@"Product"];
+    [labelproduct setTextColor:[UIColor blackColor]];
+    [labelproduct setFont:[UIFont fontWithName:@"Arial" size:14]];
+    labelproduct.sd_layout
+    .leftSpaceToView(self.view, 50/frameWidth*viewX)
+    .topSpaceToView(self.view,1000/frameHeight*viewY)
+    .widthIs(400/frameWidth*viewX)
+    .heightIs(50/frameHeight*viewY);
+    [labelproduct setTextAlignment:NSTextAlignmentLeft];
     
     
+    UILabel *txtproduct= [UILabel new];
+    [self.view addSubview:txtproduct];
+    [txtproduct setText:@""];
+    [txtproduct setTextColor:[UIColor blackColor]];
+    [txtproduct setFont:[UIFont fontWithName:@"Arial" size:14]];
+    txtproduct.sd_layout
+        .leftSpaceToView(self.view, 310/frameWidth*viewX)
+        .topSpaceToView(self.view, 1000/frameHeight*viewY)
+        .widthIs(400/frameWidth*viewX)
+        .heightIs(50/frameHeight*viewY);
+    [txtproduct setTextAlignment:NSTextAlignmentLeft];
+    [txtproduct setText:self.userNmae];
+  
+
+    //产品序列号
+    UILabel *labelserial = [UILabel new];
+    [self.view addSubview:labelserial];
+    [labelserial setText:@"Serial Number"];
+    [labelserial setTextColor:[UIColor blackColor]];
+    [labelserial setFont:[UIFont fontWithName:@"Arial" size:14]];
+    labelserial.sd_layout
+    .leftSpaceToView(self.view, 50/frameWidth*viewX)
+    .topSpaceToView(self.view, 1100/frameHeight*viewY)
+    .widthIs(400/frameWidth*viewX)
+    .heightIs(50/frameHeight*viewY);
+    [labelserial setTextAlignment:NSTextAlignmentLeft];
+    
+    UILabel *txtserial = [UILabel new];
+    [self.view addSubview:txtserial];
+    [txtserial setText:@""];
+    [txtserial setTextColor:[UIColor blackColor]];
+    [txtserial setFont:[UIFont fontWithName:@"Arial" size:14]];
+    txtserial.sd_layout
+        .leftSpaceToView(self.view, 310/frameWidth*viewX)
+        .topSpaceToView(self.view, 1100/frameHeight*viewY)
+        .widthIs(400/frameWidth*viewX)
+        .heightIs(50/frameHeight*viewY);
+    [txtserial setTextAlignment:NSTextAlignmentLeft];
+    [txtserial setText:self.phone];
+
+    //日期
+    UILabel *labeldate = [UILabel new];
+    [self.view addSubview:labeldate];
+    [labeldate setText:@"Registion Time"];
+    [labeldate setTextColor:[UIColor blackColor]];
+    [labeldate setFont:[UIFont fontWithName:@"Arial" size:14]];
+    labeldate.sd_layout
+    .leftSpaceToView(self.view, 50/frameWidth*viewX)
+    .topSpaceToView(self.view, 1200/frameHeight*viewY)
+    .widthIs(400/frameWidth*viewX)
+    .heightIs(50/frameHeight*viewY);
+    [labeldate setTextAlignment:NSTextAlignmentLeft];
+    
+    UILabel *txtdate = [UILabel new];
+    [self.view addSubview:txtdate];
+    [txtdate setText:@""];
+    [txtdate setTextColor:[UIColor blackColor]];
+    [txtdate setFont:[UIFont fontWithName:@"Arial" size:14]];
+    txtdate.sd_layout
+        .leftSpaceToView(self.view, 310/frameWidth*viewX)
+        .topSpaceToView(self.view, 1200/frameHeight*viewY)
+        .widthIs(400/frameWidth*viewX)
+        .heightIs(50/frameHeight*viewY);
+    [txtdate setTextAlignment:NSTextAlignmentLeft];
+    [txtdate setText:self.email];
+  
     
     //确认按钮
-    UIButton *btconfirm = [UIButton new];
-    [self.view addSubview:btconfirm];
+    UIButton *btmanual = [UIButton new];
+    [self.view addSubview:btmanual];
     //[btconfirm setBackgroundColor:[UIColor blueColor]];
-    [btconfirm setTitle:@"Submit" forState:UIControlStateNormal];
-    [btconfirm setSd_cornerRadius:@10.0];
-    btconfirm.sd_layout
+    [btmanual setTitle:@"Production Manual" forState:UIControlStateNormal];
+    [btmanual setSd_cornerRadius:@10.0];
+    btmanual.sd_layout
     .centerXEqualToView(self.view)
     .widthIs(500/frameWidth*viewX)
-    .topSpaceToView(self.view, 1050/frameHeight*viewY)
+    .topSpaceToView(self.view, 1330/frameHeight*viewY)
     .heightIs(90/frameHeight*viewY);
-    [btconfirm setBackgroundColor:[UIColor colorWithRed:29.0/255 green:130.0/255 blue:254.0/255 alpha:1.0]];
-    [btconfirm addTarget:self action:@selector(goinfo) forControlEvents:UIControlEventTouchUpInside];
+    [btmanual setBackgroundColor:[UIColor colorWithRed:29.0/255 green:130.0/255 blue:254.0/255 alpha:1.0]];
+  //  [btmanual addTarget:self action:@selector(goinfo) forControlEvents:UIControlEventTouchUpInside];
     
     
 }
